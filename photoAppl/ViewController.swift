a092ef37d19103a6e0180bd8a59bade29e95cc7f
Ioannis Karagogos <ikaragogos@gmail.com>
2017-03-27 15:12:00 +0300

    before in Load

--- photoAppl/ViewController.swift
+++ photoAppl/ViewController.swift
@@ -7,19 +7,101 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
 
+    
+    var picker: UIImagePickerController?
+    @IBOutlet weak var imageIn: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    //MARK: -ShowCameraInterface
+    
+    @IBAction func showImagePickerForCamera(_ sender: Any) {
+        let authStatus = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+        if (authStatus == .denied){
+            
+            let alertController = UIAlertController(title: "Unable to access the Camera",
+             message: "To enable access, got to Settings > Privacy > Camera and turn on Camera access for this app",
+             preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(ok)
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else if (authStatus == .notDetermined){
+            
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { granted in
+                if granted {self.showImagePickerForSourceType(sourceType: .camera, button: sender as! UIBarButtonItem)}
+            })
+            
+        } else {
+            self.showImagePickerForSourceType(sourceType: .camera, button: sender  as! UIBarButtonItem)
+        }
+    }
+    
+    
+    func showImagePickerForSourceType(sourceType:UIImagePickerControllerSourceType, button:UIBarButtonItem){
+        
+        if (imageIn.isAnimating) {
+            imageIn.stopAnimating()
+        }
+        let picker = UIImagePickerController()
+        picker.modalPresentationStyle = .currentContext
+        picker.sourceType = sourceType
+        picker.delegate = self
+        picker.modalPresentationStyle = .fullScreen
+        
+        let presenter = picker.popoverPresentationController
+        presenter?.barButtonItem = button
+        presenter?.permittedArrowDirections = .any
+        
+        self.picker = picker// not unserstood
+        self.present(self.picker!, animated: true, completion: nil)// not unserstood
+    }
+    
+    
+    //MARK: -UIImagePickerController delegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage]
+        print("Captured image \(image)")
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print(" Cancel taking photo ")
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
+   
